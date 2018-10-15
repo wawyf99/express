f1baf0d0da0ef6b3db1832175e05f3a6c1c18e3b
unknown <star.cheung@live.cn>
2018-10-15 20:02:23 +0800

    s

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.all('*', function (req, res, next) {
         }
     }
 
+
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
